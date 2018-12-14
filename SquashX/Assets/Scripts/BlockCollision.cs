129287dc3570df513fbc21be0c71ca1d9c548e17
Konisko Bandyopadhyay <30558121+Jakoban2411@users.noreply.github.com>
2018-12-14 15:29:27 +0000

    Particle Effect added

--- SquashX/Assets/Scripts/BlockCollision.cs
+++ SquashX/Assets/Scripts/BlockCollision.cs
@@ -8,34 +8,43 @@ public class BlockCollision : MonoBehaviour {
     [SerializeField] int PointsToAdd = 10;
     BlockCount blockCount;
     ParticleSystem ThanosSnap;
+    Animator ScaleDown;
     Sprite particlesprite;
+    int i = 150,j=0;
     public GameObject ParticleEffectObject;
     // Use this for initialization
     void Start ()
     {
+        ScaleDown = GetComponent<Animator>();
         GameObject CreatedPrefab=Instantiate(ParticleEffectObject, transform.position, transform.rotation);
         ThanosSnap=CreatedPrefab.GetComponent<ParticleSystem>();
         blockCount = FindObjectOfType<BlockCount>();
         blockCount.IncBlockNo();
-        SpriteRenderer something=GetComponent<SpriteRenderer>();
+        SpriteRenderer something =GetComponent<SpriteRenderer>();
         particlesprite=something.sprite;
         ThanosSnap.textureSheetAnimation.SetSprite(0, particlesprite);
+        
     }
 
     public void OnCollisionExit2D(Collision2D collision)
     {
+        if (ScaleDown)
+            ScaleDown.enabled = true;
         if (!bSoundPlayed)
         {
             AudioSource.PlayClipAtPoint(crunchsound, Camera.main.transform.position);
             bSoundPlayed = true;
         }
         blockCount.AddToScore(PointsToAdd);
-        Destroy(gameObject,crunchsound.length);
+        
+        ScaleDown.Play("Shrink");
+       // Destroy(gameObject,crunchsound.length);
     }
     private void OnDestroy()
     {
         if(ThanosSnap)
         ThanosSnap.Play();
+       
         blockCount.DecBlock();
         //Destroy(ParticleEffectObject, 5);
         
@@ -44,5 +53,18 @@ public class BlockCollision : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (j == i)
+        {
+            if (ScaleDown)
+            {
+                ScaleDown.enabled = true; ScaleDown.Play("Shrink");
+
+                //ScaleDown.SetBool("Shrink", true);
+            }
+            Debug.Log("Anumation shouldve startetd");
+        }
+            j++;
+        Debug.Log("J=" + j);
+        
     }
 }
